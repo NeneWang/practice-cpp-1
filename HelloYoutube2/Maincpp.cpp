c4dec6c6f433a84c65b6f6ac6f227a74f49ce278
NelsonWang <wangnelson2@gmail.com>
2022-08-27 08:35:38 -0400

    appending | vars

--- HelloYoutube2/Maincpp.cpp
+++ HelloYoutube2/Maincpp.cpp
@@ -11,10 +11,14 @@ int main() {
 	double price = 9999.99;
 	char status = 'N';
 	bool for_sale = true;
+	string make = "Ford";
 	string model = "Mustang";
+
+	string car = make + " " + model;
 
 	cout << "Year: " << year << endl;
 	cout << "Price: " << price << endl;
+	cout << "Model: " << car << endl;
 	cout << "New/Used: " << status << endl;
 	cout << "is onsale " <<  for_sale << endl;
 
