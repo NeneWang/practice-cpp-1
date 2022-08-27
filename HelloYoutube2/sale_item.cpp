347103232c1dc9001884c8544c427177452717bb
NelsonWang <wangnelson2@gmail.com>
2022-08-27 08:51:16 -0400

    crashcourse | 4

--- HelloYoutube2/sale_item.cpp
+++ HelloYoutube2/sale_item.cpp
@@ -7,9 +7,25 @@ using namespace std;
 int main() {
 	string item;
 	double price;
+	int quantity;
+	double total;
+
+	cout << "What item did you buy?";
+	getline(cin, item);
+	cout << "What is the price for each? $";
+	cin >> price;
+	cout << "How many do you want to buy?:";
+	cin >> quantity;
+	cout << endl;
+
+	total = price * quantity;
+	
+	cout << "item: " << item << endl;
+	cout << "price: " << price << endl;
+	cout << "x " << quantity << endl;
 
 
-
+	cout << endl;
 	return 0;
 }
 
