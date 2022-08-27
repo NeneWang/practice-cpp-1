ce2ad0c035d45eeaaf0856d09712d0384256e8f9
NelsonWang <wangnelson2@gmail.com>
2022-08-27 08:24:36 -0400

    variables

--- HelloYoutube2/Maincpp.cpp
+++ HelloYoutube2/Maincpp.cpp
@@ -6,5 +6,14 @@ int main() {
 	std::cout << "Never gonna give you up\n";
 	std::cout << "Never gonna let you down!\n";
 
+
+	int year = 2020;
+	double price = 9999.99;
+	char status = 'N';
+	bool for_sale = true;
+
+	cout << year << endl;
+	cout << price << endl;
+
 	return 0;
 }
