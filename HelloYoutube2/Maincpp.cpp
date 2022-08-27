7493b9ba3090e9825a07941e7a081af8b0f6974a
NelsonWang <wangnelson2@gmail.com>
2022-08-27 08:18:27 -0400

    never gonna |

--- HelloYoutube2/Maincpp.cpp
+++ HelloYoutube2/Maincpp.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-	std::cout << "Never gonna give you up";
-	std::cout << "Never gonna let you down!";
+	std::cout << "Never gonna give you up\n";
+	std::cout << "Never gonna let you down!\n";
 
-
+	return 0;
 }
